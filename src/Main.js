dcb72dfff711bf65b753b278476695923f19a551
venderjin <lffpz3331@gmail.com>
2023-10-18 01:47:41 +0900

    main background img change over time

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,32 @@
 import React, { useState, useEffect } from "react";
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Home from "./Home";
-import mainBackground from "./asset/mainMap4.png";
+import mainMap_night from "./asset/mainMap_night.png";
+import mainMap_daytime from "./asset/mainMap_daytime.png";
+import mainMap_evenig from "./asset/mainMap_evenig.png";
+
 // import mainLogo from "./asset/desp-online logo.png";
 
+// const CurrentHour = () => {
+//     const hour = new Date().getHours();
+//     console.log(hour);
+// };
+
 const Main = () => {
-    let cnt = 0;
-    setInterval(() => console.log(++cnt), 2000);
+    const [hour, setHour] = useState(new Date().getHours());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const currentHour = new Date().getHours();
+            setHour(currentHour);
+            console.log("Current Hour:", currentHour);
+        }, 10000); // 10초(10,000 밀리초)마다 실행
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []); // 빈 배열을 사용하여 이펙트가 초기 한 번만 실행되도록 설정
+    const mainMap = hour >= 7 && hour < 16 ? mainMap_daytime : hour >= 16 && hour < 20 ? mainMap_evenig : mainMap_night;
 
     const [width, setWidth] = useState(window.innerWidth); // 화면 너비를 초기화
     const [height, setHeight] = useState(window.innerHeight); // 화면 높이를 초기화
@@ -38,7 +58,7 @@ const Main = () => {
         width: width,
         height: height,
         filter: "blur(3px)", // 이미지에 5px 블러 효과 적용
-        backgroundImage: `url(${mainBackground})`,
+        backgroundImage: `url(${mainMap})`,
         backgroundPosition: "center",
         backgroundSize: "cover", // 이미지를
         overflowX: "auto", // 수평 스크롤을 자동으로 추가
