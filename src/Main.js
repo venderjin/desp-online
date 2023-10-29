408c06630dd03c60eb2ebc8256823f75718cbb48
venderjin <lffpz3331@gmail.com>
2023-10-29 18:58:55 +0900

    img load from AWS S3

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,25 @@
 import React, { useState, useEffect } from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./Home";
-import mainMap_night from "./asset/mainMap_night.png";
-import mainMap_daytime from "./asset/mainMap_daytime.png";
-import mainMap_evenig from "./asset/mainMap_evenig.png";
-
-// import mainLogo from "./asset/desp-online logo.png";
-
-// const CurrentHour = () => {
-//     const hour = new Date().getHours();
-//     console.log(hour);
-// };
+import Home from "./Home";
 
 const Main = () => {
+    //동적UI 구성을 위한 상태관리
     const [hour, setHour] = useState(new Date().getHours());
+
+    const mainMap_night = "https://despbukkit.s3.ap-northeast-2.amazonaws.com/mainMap_night.jpg";
+    const mainMap_daytime = "https://despbukkit.s3.ap-northeast-2.amazonaws.com/mainMap_daytime.jpg";
+    const mainMap_evening = "https://despbukkit.s3.ap-northeast-2.amazonaws.com/mainMap_evening.jpg";
 
     useEffect(() => {
         const interval = setInterval(() => {
             const currentHour = new Date().getHours();
             setHour(currentHour);
-            console.log("Current Hour:", currentHour);
         }, 10000); // 10초(10,000 밀리초)마다 실행
 
         return () => {
             clearInterval(interval);
         };
     }, []); // 빈 배열을 사용하여 이펙트가 초기 한 번만 실행되도록 설정
-    const mainMap = hour >= 7 && hour < 16 ? mainMap_daytime : hour >= 16 && hour < 20 ? mainMap_evenig : mainMap_night;
+    const mainMap = hour >= 7 && hour < 16 ? mainMap_daytime : hour >= 16 && hour < 20 ? mainMap_evening : mainMap_night;
 
     const [width, setWidth] = useState(window.innerWidth); // 화면 너비를 초기화
     const [height, setHeight] = useState(window.innerHeight); // 화면 높이를 초기화
@@ -43,6 +36,26 @@ const Main = () => {
             window.removeEventListener("resize", handleResize);
         };
     }, []);
+
+    //메뉴 Tab 구성
+    const [currnetTab, setCurrentTab] = useState(0);
+
+    // const menuArr = [
+    //     { name: "공지사항", content: <AdminMemberList /> },
+    //     { name: "캐릭터 가이드", content: <AdimWdList /> },
+    //     { name: "홈", content: <AdminMemberList /> },
+    //     { name: "랭킹", content: <AdminMemberList /> },
+    //     { name: "유저검색", content: <AdminMemberList /> },
+    //     { 후원: "커뮤니티", content: <AdminMemberList /> },
+    // ];
+
+    const selectMenuHandler = (index: any) => {
+        setCurrentTab(index);
+    };
+
+    // useEffect(() => {
+    //     console.log(width, height);
+    // }, [width, height]);
 
     const containerBackStyle = {
         backgroundColor: "#231C0D",
@@ -77,11 +90,31 @@ const Main = () => {
         zIndex: 2, // 이미지를 위로 올림
     };
 
+    const contentLayoutStyle = {
+        height: height,
+        width: width > 1000 ? width / 1.5 : width,
+        // backgroundColor: "red",
+    };
+
+    const tabMenuStyle = {
+        height: height / 7,
+        width: width > 1000 ? width / 1.5 : width,
+        borderBottomColor: "white",
+        borderBottom: "2px solid white",
+        justifyContent: "center", // 가로 중앙 정렬
+        alignItems: "center", // 세로 중앙 정렬
+    };
+
     return (
         <div style={containerBackStyle}>
             <div style={containerStyle}></div>
             <div style={containerCoverStyle}>
-                <h1 style={{ color: "white" }}>desp online</h1>
+                <div style={contentLayoutStyle}>
+                    <div style={tabMenuStyle}>
+                        <h1>메뉴바</h1>
+                    </div>
+                    <Home />
+                </div>
             </div>
         </div>
     );
