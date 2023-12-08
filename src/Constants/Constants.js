d0b043e64e49bf8add61b0e4f1f8526e1abcde84
venderjin <lffpz3331@gmail.com>
2023-12-08 22:36:53 +0900

    fix(): 결제약관동의  toggle 생성

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,10 +1,10 @@
 const backEndUri = {
-    rankingPVP: "http://121.135.44.209:8080/ranking/pvp",
-    rankingLevel: "http://121.135.44.209:8080/ranking/level",
-    searchPlayer: "http://121.135.44.209:8080/search/player/",
-    nicknameValidation: "http://121.135.44.209:8080/cash/existuser/",
-    charge: "http://121.135.44.209:8080/cash/charge",
-    chargeLog: "http://121.135.44.209:8080/cash/chargelog",
+    rankingPVP: "/ranking/pvp",
+    rankingLevel: "/ranking/level",
+    searchPlayer: "/search/player/",
+    nicknameValidation: "/cash/existuser/",
+    charge: "/cash/charge",
+    chargeLog: "/cash/chargelog",
 };
 
 export default backEndUri;
